09045f1e34fcdca3f652cab17ce9da4048d0eee5
asalas <67822688+andrewjsalas@users.noreply.github.com>
2022-05-07 15:38:03 -0700

    Create Whiteboard Card and Style Home Page Header

--- src/components/whiteBoard.jsx
+++ src/components/whiteBoard.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { Card, Button } from 'react-bootstrap'
 
 const whiteBoard = () => {
   return (
-    <div>whiteBoard</div>
+    <div className='container'>
+      <Card className='whiteboard-card text-center' style={{width: '28rem'}}>
+        <Card.Body >
+          <Card.Title className='whiteboard-title'>My Whiteboard</Card.Title>
+          <Card.Text className='whiteboard-text'>
+            This is your Whiteboard. Each whiteboard represents its own workout 
+            plan. Hit 'Create' to start a blank whiteboard. 
+          </Card.Text>
+          <Button varient='primary'>Create</Button>
+        </Card.Body>
+      </Card>
+    </div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import WhiteBoard from '../components/WhiteBoard'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='container'>
+      <div className='header'>
+        <h1 className='home-header display-6'>Welcome to,</h1>
+        <h1 className='home-header-title display-3'>Workout Creator</h1>
+      </div>
+      
+      <WhiteBoard />
+      
+    </div>
   )
 }
 
